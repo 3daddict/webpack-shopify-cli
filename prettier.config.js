7c27fd75db5ea9e276f524e9d2edd4ff7a5178a1
Mike Salvati <cdnmikes@gmail.com>
2022-04-14 08:02:50 -0700

    formatting setup update

--- prettier.config.js
+++ prettier.config.js
@@ -1,26 +1,25 @@
 module.exports = {
-    singleQuote: true,
-    trailingComma: 'all',
-    printWidth: 80,
-    proseWrap: 'always',
-    tabWidth: 2,
-    useTabs: false,
-    bracketSpacing: true,
-    semi: true,
-    arrowParens: 'avoid',
-    overrides: [
-      {
-        files: ['.prettierrc', '.eslintrc'],
-        options: {
-          parser: 'json',
-        },
+  singleQuote: true,
+  trailingComma: "all",
+  printWidth: 80,
+  proseWrap: "always",
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  semi: true,
+  arrowParens: "avoid",
+  overrides: [
+    {
+      files: [".prettierrc", ".eslintrc"],
+      options: {
+        parser: "json",
       },
-      {
-        files: ['package.json'],
-        options: {
-          printWidth: 180,
-        },
+    },
+    {
+      files: ["package.json"],
+      options: {
+        printWidth: 180,
       },
-    ],
-  };
-  
+    },
+  ],
+};
