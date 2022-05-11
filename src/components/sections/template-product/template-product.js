664beb7c45025fcbc3f0ecce463e866dc2c26242
Mike Salvati <cdnmikes@gmail.com>
2022-05-11 09:42:16 -0700

    product template compeleted

--- src/components/sections/template-product/template-product.js
+++ src/components/sections/template-product/template-product.js
@@ -1,8 +1,98 @@
-import Swiper from 'swiper';
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-const swiper = new Swiper('.mySwiper', {
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  // configure Swiper settings
+  direction: 'horizontal',
   pagination: {
     el: '.swiper-pagination',
   },
+  navigation: {
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+  },
 });
+
+console.log('Swiper Loaded', swiper);
+// docs: https://swiperjs.com/get-started#install-from-npm
+
+class VariantSelector extends HTMLElement {
+  constructor() {
+    super();
+    this.addEventListener('change', this.onVariantChange);
+  }
+
+  onVariantChange() {
+    this.getSelectedOptions();
+    this.getSelectedVariant();
+    if (this.currentVariant) {
+      this.updateURL();
+      this.updateFormID();
+      this.updatePrice();
+    }
+  }
+
+  getSelectedOptions() {
+    this.options = Array.from(
+      this.querySelectorAll('select'),
+      select => select.value,
+    );
+    console.log(this.options);
+  }
+
+  getVariantJSON() {
+    this.variantData =
+      this.variantData ||
+      JSON.parse(this.querySelector('[type="application/json"]').textContent);
+
+    return this.variantData;
+  }
+
+  getSelectedVariant() {
+    this.currentVariant = this.getVariantJSON().find(variant => {
+      const findings = !variant.options
+        .map((option, index) => {
+          return this.options[index] === option;
+        })
+        .includes(false);
+      if (findings) return variant;
+    });
+    console.log(this.currentVariant);
+  }
+
+  updateURL() {
+    if (!this.currentVariant) return;
+    window.history.replaceState(
+      {},
+      '',
+      `${this.dataset.url}?variant=${this.currentVariant.id}`,
+    );
+  }
+  updateFormID() {
+    const form_input = document
+      .querySelector('#product-form')
+      .querySelector('input[name="id"]');
+    form_input.value = this.currentVariant.id;
+  }
+  updatePrice() {
+    fetch(
+      `${this.dataset.url}?variant=${this.currentVariant.id}&section_id=${this.dataset.section}`,
+    )
+      .then(response => response.text())
+      .then(responseText => {
+        const id = `price-${this.dataset.section}`;
+        const html = new DOMParser().parseFromString(responseText, 'text/html');
+        const oldPrice = document.getElementById(id);
+        const newPrice = html.getElementById(id);
+        if (oldPrice && newPrice) oldPrice.innerHTML = newPrice.innerHTML;
+      });
+  }
+}
+
+customElements.define('variant-selector', VariantSelector);
